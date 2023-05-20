44025b82154f71f4b831a8369d8f81a996143fed
Bruno <brunocosta.1991@hotmail.com>
2023-05-19 22:58:05 -0300

    variavies

---
+++ 04_variaveis/01_criacao_de_var/index.php
@@ -0,0 +1,15 @@
+<?php
+
+$teste = "Algum texto";
+echo $teste;
+
+$num = 10;
+echo "<br>";
+echo $num;
+
+$_nome = "Bruno";
+echo $_nome;
+
+echo "<br>";
+$velicidadeMaxima = 100;
+$velicidade_minima = 100;
---
+++ 04_variaveis/02_execicio_08/index.php
@@ -0,0 +1,11 @@
+<?php
+
+$velocidade = 100;
+$marca = "Ferrari";
+$itens = ['teto_solar', 'motor 2.0', "porta malas grande", 'piloto automatico'];
+
+echo $velocidade;
+echo "<br>";
+echo $marca;
+echo "<br>";
+print_r($itens);
