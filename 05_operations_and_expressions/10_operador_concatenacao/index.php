a7117534f61b149eaaeb12900f42c6a2ed894538
Bruno <brunocosta.1991@hotmail.com>
2023-05-22 20:16:49 -0300

    Operadores e expressao

---
+++ 05_operations_and_expressions/10_operador_concatenacao/index.php
@@ -0,0 +1,17 @@
+<?php
+
+echo "testando a concatenação";
+echo "<br>";
+echo "Testando" . " a " . "concatenação";
+echo "<br>";
+$t = "testando";
+$c = "concatenação";
+echo $t . " a " . $c;
+echo "<br>";
+
+$marca = "Ferrari";
+$motor = "3.0";
+$velMax = 200;
+
+echo "O carro da " . $marca . " tem um motor " . $motor . " e chega uma velocidade de " . $velMax . "km/h";
+echo "<br>";
